1099466ab4d43506d3c736989e3e5a49a3529b97
elliotastern <stern.elliot@gmail.com>
2021-04-14 17:26:49 -0700

    clean v1

--- play-by-play-2020.R
+++ play-by-play-2020.R
@@ -17,14 +17,55 @@ color_pallette <- c("#069E2D", "#DB162F")
 ############################## 
 # 1 - LOAD DATA
 ##############################
-nba_player_box_score_df <- read.csv("/Users/elliotstern/OneDrive/Sports/nba/NBA_Play_by_Play/2021_games_details.csv",
+nba_player_box_score_df <- read.csv("2020_games_details.csv",
                                     stringsAsFactors = FALSE)
 
-nba_team_box_score_df <- read.csv("/Users/elliotstern/OneDrive/Sports/nba/NBA_Play_by_Play/2021_box_scores.csv",
+nba_team_box_score_df <- read.csv("2020_box_scores.csv",
                                   stringsAsFactors = FALSE)
 
-player_details_df <- read.csv("/Users/elliotstern/OneDrive/Sports/nba/nba_2021_player_details.csv",
+player_details_df <- read.csv("nba_2020_player_details.csv",
                               stringsAsFactors = FALSE)
 
-player_name_translator <- read.csv("/Users/elliotstern/OneDrive/Sports/nba/NBA_Details/translator_player_names.csv",
+player_name_translator <- read.csv("translator_player_names.csv",
                                    stringsAsFactors = FALSE)
+
+
+############################## 
+# 2 - DATA CLEANING
+############################## 
+## CLEAN DATAFRAMES
+# make the date columns the same in both dataframes
+nba_team_box_score_df$GAME_DATE_EST <- as.Date(nba_team_box_score_df$GAME_DATE_EST, format = "%m/%d/%y")
+nba_player_box_score_df$Date <- as.Date(nba_player_box_score_df$Date, format = "%m/%d/%y")
+
+# remove players who did not play
+nba_player_box_score_df <- nba_player_box_score_df[!is.na(nba_player_box_score_df$FG3M),]
+# create data team id to group by so we can create lineups for each game
+nba_player_box_score_df$DATE_TEAM_ID <- paste(nba_player_box_score_df$Date, nba_player_box_score_df$TEAM_ABBREVIATION)
+# get the lineup by for each team and date
+nba_player_box_score_df %<>%
+  group_by(DATE_TEAM_ID) %>%
+  mutate(lineup = list(PLAYER_NAME))
+# create a dataframe of playing lineup for each team on each date
+lineup_df <- nba_player_box_score_df[, c("Date", "TEAM_ABBREVIATION", "lineup", "DATE_TEAM_ID")]
+lineup_df <- distinct(lineup_df, DATE_TEAM_ID, .keep_all = TRUE)
+
+
+## CREATE DATAFRAME of players who played
+# create a dataframe of all nba players and their teams so we can connect the data
+nba_player_list <- nba_player_box_score_df[, c("PLAYER_NAME", "TEAM_ABBREVIATION")] 
+nba_player_list <- distinct(nba_player_list, PLAYER_NAME, TEAM_ABBREVIATION)
+nba_player_list$PLAYER_NAME_underlined <- gsub(" ", "_", nba_player_list$PLAYER_NAME)
+# View(nba_player_list)
+
+
+
+# clean the team box scores so each team has self and opponent stats in each row.
+nba_team_box_score_df_away_teams <- nba_team_box_score_df 
+nba_team_box_score_df <- nba_team_box_score_df[, c("GAME_DATE_EST", "REAL_GAME_ID",
+                                                   "Initials","nickname", "Initials_away",
+                                                   "PTS_home", "FG_PCT_home", 
+                                                   "FG3_PCT_home", "AST_home", 
+                                                   "REB_home", "PTS_away", 
+                                                   "FG_PCT_away", "FG3_PCT_away", 
+                                                   "AST_away", "REB_away")]
